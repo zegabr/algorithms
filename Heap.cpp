40a1490208056708a8af0d4c6857e1caed3a2199
Jose Gabriel Silva Pereira <jgsp2@ug4c18.windows.cin.ufpe.br>
2018-03-19 12:58:29 -0300

    implementing Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct heap{
-    int arr[100];
-    int n=0;//will be the current element quantity
-    heap(){
-
-    }
+struct Heap{
+    int arr[1000000];
+    int n;//will be the current element quantity
+    
     
 }typedef Heap;
 
@@ -14,22 +12,53 @@ void swap(int *a,int *b){
     int aux = *a;*a=*b;*b=aux;
 }
 
-int leftkid(int n,int parent){
-    int left = parent*2+1;
-    if(left<=n)
+int leftkid(Heap *H,int parent){
+    int left = (parent*2+1);
+    if(left<=H->n)
         return left;
     else
         return -1;
     }
-int rightkid(int n,int parent){
-    int right = parent*2+2;
-    if(left<=n)
-        return left;
+int rightkid(Heap *H,int parent){
+    int right = (parent*2+2);
+    if(right<=H->n)
+        return right;
     else
         return -1;
     
 }
+void siftUp(Heap *H, int index){
 
+}
+void siftDown(Heap *H, int index){
+
+}
+void insert(Heap *H, int key){
+    if(H->n+1<1000000){
+        H->arr[H->n]=key;
+        H->n++;
+        siftUp(H, H->n-1);
+    }
+}
+int remove(Heap *H){
+    int key = H->arr[0];
+    H->arr[0] = H->arr[H->n];
+    H->n--;
+    siftDown(H,0);
+    return key;
+}
+void heapfy(Heap *H){
+
+}
+void heapsort(Heap *H){
+
+}
+void print(Heap *H){
+    for(int i= 0;i<=H->n;i++)
+        cout<<H->arr[i]<<" ";
+
+    cout<<endl;
+}
 
 int main(){
     Heap *H = new Heap();
