2ae2e590551c430067ce4b0ce8973518d01a16e8
Jose Gabriel Silva Pereira <jgsp2@ug3c29.windows.cin.ufpe.br>
2018-03-24 08:01:36 -0300

    stack and queue c++ with pointers implemented

--- Queue.cpp
+++ Queue.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 struct node{
     int key;
@@ -8,6 +8,7 @@ struct filha{
     node *cabeca,*bunda;
 
 };
+
 filha *inserir(filha *q,int key){
         node *aux = new node();
         aux->key=key;
@@ -21,20 +22,35 @@ filha *inserir(filha *q,int key){
         return q;
     }
 }
+
 filha *remover(filha *q){
     if(q->cabeca!=NULL)
         q->cabeca=q->cabeca->next;
     else
         cout<<"cannot remove because it is empty, you can cry now\n";
+
+    if(q->cabeca==NULL)//if its empty
+        q->bunda=NULL;
     return q;
 }
+
 void imprime(filha *q){
     node *aux = q->cabeca;
+    cout<<"cabeca<---->bunda\n";
     while(aux!=NULL){
         cout<<aux->key<<" ";
         aux=aux->next;
     }
     cout<<endl;
+}
+bool semCabeca(filha *q){
+    return q->cabeca==NULL ? true : false;
+}
+int quantotemnacabeca(filha *q){
+    return q->cabeca->key;
+}
+int quantotemnabunda(filha *q){
+    return q->bunda->key;
 }
 
 int main(){
