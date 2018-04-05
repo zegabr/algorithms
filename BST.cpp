ca0c84d7abaf816a63bdcbce61cb851e06afae93
Jose Gabriel Silva Pereira <jgsp2@ug2c27.windows.cin.ufpe.br>
2018-04-05 08:15:07 -0300

    BST remove function added

--- BST.cpp
+++ BST.cpp
@@ -4,11 +4,11 @@ using namespace std;
 struct node{
     int key;
     struct node *left,*right;
-}typedef Node;
+};
 
-Node * insert(Node *root, int key){
+node *insert(node *root, int key){
     if(root==NULL){
-        root = new Node;
+        root = new node;
         root->key=key;
         root->left=root->right=NULL;
     }else if(key<root->key){
@@ -18,23 +18,78 @@ Node * insert(Node *root, int key){
     }
         return root;
 }
-void posOrder(Node *raiz) {
+void inorder(node *raiz) {
 	if(raiz != NULL) {
-		posOrder(raiz->left);
-		posOrder(raiz->right);
-		cout << raiz->key << endl;
+		inorder(raiz->left);
+		cout << raiz->key << " ";
+		inorder(raiz->right);
 	}
 }
+node *del(node *root,int key){
+    if(root==NULL)
+        return root;
+    else{
+        if(key<root->key)
+            root->left = del(root->left,key);
+        else if(key>root->key)
+            root->right = del(root->right,key);
+        else{//key == root->key
+            if(root->right==NULL && root->left==NULL)//no child
+                return NULL;
+            else{//one or two child
+                if(root->left==NULL)//only right child
+                    return root->right;
+                else if(root->right==NULL)
+                    return root->left;
+                else{//has two childs
+                    node * aux = root->right;
+                    while(aux->left!=NULL)
+                        aux=aux->left;
+                    //now aux is inorder successor
+                    root->key=aux->key;
+                    root->right = del(root->right,root->key);
+                }
+            }
 
-int main(){
-    int key;
-    Node *tree=NULL;
-    while(cin >> key){
-	
-        tree=insert(tree,key);
-	
+        }
+
+
     }
-    posOrder(tree);
-
+    return root;
+}
+int main(){
+    /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+    struct node *root = NULL;
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+ 
+    cout<<"Inorder traversal of the given tree \n";
+    inorder(root);
+ 
+    cout<<"\nDelete 20\n";
+    root = del(root, 20);
+    cout<<"Inorder traversal of the modified tree \n";
+    inorder(root);
+ 
+    cout<<"\nDelete 30\n";
+    root = del(root, 30);
+    cout<<"Inorder traversal of the modified tree \n";
+    inorder(root);
+ 
+    cout<<"\nDelete 50\n";
+    root = del(root, 50);
+    cout<<"Inorder traversal of the modified tree \n";
+    inorder(root);
+ 
     return 0;
 }
