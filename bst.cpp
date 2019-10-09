39a45e87182de377017be397bf7c90302db576ff
Jose Gabriel Silva Pereira <jgsp2@ug2c29.windows.cin.ufpe.br>
2019-10-09 16:53:24 -0300

    bst root bugado???

---
+++ bst.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+#define null NULL
+
+
+template<class T>
+class node{
+	public:
+		T info;
+		node *left, *right;
+		node(T val){
+			info = val;
+			left=right=null;
+		}
+};
+
+template<class T>
+class bst{
+	private:
+		node<T> *root;
+
+	public:
+		bst(){root=null;}
+		bool empty(){
+			return root==null;
+		}
+		void insert(T val, node<T> *R = root){
+			if(empty()){
+				node<T> *n = new node<T>(val);
+				root = n;
+			}else if(val < R->info) insert(val, R->left);
+			else if(val > R->info) insert(val, R->left);
+		}
+
+		void preorder(node<T> *R = root){
+			if(root->left!=null) preorder(R->left);
+			cout<<R->info<<' ';
+			if(root->right!=null) preorder(R->right);
+
+		}
+
+};
+
+
+int main(){
+	bst<int> s;
+
+
+}
